9bb0fc241a7b12e2c7cd1d431f028a6dadfc660c
linarsy <lina.stihi@gmail.com>
2019-08-21 14:18:27 +0300

    Шаг 2 - Работа с чатом

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -1,10 +1,38 @@
+import axios from 'axios';
+import io from 'socket.io-client';
 import React from 'react';
+import getUserName from '../../lib/getUserName';
+import routes from '../routes';
 
 export default class Component extends React.Component {
-  state = {};
+  state = { ...this.props, text: '', name: getUserName() };
+
+  componentDidMount() {
+    const port = process.env.PORT;
+    const socket = io(port);
+
+    socket.on('newMessage', (data) => {
+      const { messages } = this.state;
+      this.setState({ messages: [...messages, data.data.attributes] });
+    });
+  }
+
+  handleChangeText = (e) => {
+    this.setState({ text: e.target.value });
+  };
+
+  handleSubmitForm = (e) => {
+    e.preventDefault();
+    const { currentChannelId, text, name } = this.state;
+    const url = routes.channelMessagesPath(currentChannelId);
+
+    axios.post(url, { data: { attributes: { text, author: name } } });
+    // .then(res => console.log(res.config.data))
+    // .catch(err => console.log(err));
+  }
 
   render() {
-    const { channels, messages } = this.props;
+    const { messages, channels, text } = this.state;
 
     return (
       <div className="row">
@@ -13,13 +41,19 @@ export default class Component extends React.Component {
             {channels.map(item => <li key={item.id} className="list-group-item list-group-item-info">{item.name}</li>)}
           </ul>
         </div>
-        <div className="col-md-9 my-5">
+        <div className="col-md-8 my-5 ml-auto">
           <ul className="list-group">
-            {messages.length ? null : <p>Начните беседу</p>}
+            {messages.length
+              ? messages.map(item => (
+                <div key={item.id} className="mb-4">
+                  <p className="text-info text-right font-weight-bold text-capitalize">{item.author}</p>
+                  <p className="alert alert-info">{item.text}</p>
+                </div>
+              )) : <p>Начните беседу</p>}
           </ul>
-          <form>
-            <div className="form-group">
-              <textarea className="form-control" rows="3" />
+          <form className="mt-5" onSubmit={this.handleSubmitForm}>
+            <div className="form-group mb-4">
+              <textarea className="form-control" rows="3" onChange={this.handleChangeText} value={text} />
             </div>
             <button type="submit" className="btn btn-outline-info btn-lg d-flex ml-auto px-5">Отправить</button>
           </form>
